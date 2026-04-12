0f91a859a5b66b233d87a8e4078ac2d111b3a838
Hana Adan <hanaadamabdi@gmail.com>
2026-04-12 23:43:30 +0300

    vercal added

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,7 +17,8 @@ import getCategories  from './routes/getCategries.router';
 const cors = require('cors');
 
 const corsOptions = {
-  origin: 'http://localhost:5173', 
+  // origin: 'http://localhost:5173', 
+  origin: 'https://expense-tracker-iota-snowy-75.vercel.app', 
   methods : ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true, 
 };

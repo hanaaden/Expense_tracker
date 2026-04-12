90b41de0ecafb70070fddcd7aec84dd309974d1f
Hana Adan <hanaadamabdi@gmail.com>
2026-04-12 23:48:01 +0300

    render added

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const API = axios.create({
-   baseURL : "http://localhost:3131",
+//    baseURL : "http://localhost:3131",
+   baseURL : "https://expense-tracker-4inm.onrender.com",
     withCredentials : true
 })
 export default API;
